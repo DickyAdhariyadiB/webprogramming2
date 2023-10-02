7d9b13f26a626ddbb1669f7c7a79b07a6342221a
DickyAdhariyadiB <dickyadhariyadi@gmail.com>
2023-10-02 16:57:15 +0700

    tugas 2 pertemuan3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Dicky Adhariyadi
+        Saya tinggal di Daerah Jakarta Pusat
+        Olahraga yang saya sukai adalah Sepak bola";
+    }
+}
