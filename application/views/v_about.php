3854684a0c39e04084b08ecf6fbc01e3a1edc6f8
DickyAdhariyadiB <dickyadhariyadi@gmail.com>
2023-10-15 13:20:46 +0700

    pertemuan4

---
+++ application/views/v_about.php
@@ -0,0 +1,33 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>NAMA</h4>
+
+    <ul type="disc">
+
+    <li>NAMA DEPAN : DICKY ADHARIYADI </li>
+
+    <li>NAMA BELAKANG: BHAKTI</li>
+
+    </ul>
+
+    <br>
+
+    <h4>ALAMAT</h4>
+
+    <ul type="disc">
+
+    <li> JAKARTA, INDONESIA</li> </ul>
+
+    <h4>TEMPAT LAHIR</h4>
+
+    <ul type="disc"> <li>JAKARTA</li>
+
+    </ul>
+
+    <h4>OLAHRAGA FAVORIT</h4> <ul type="square"> <li>SEPAK TAKRAW</li>
+
+    <li>CATUR</li>
+
+    </ul>
+
+    </section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <a href="http://www.RENTALBUKU.com">RENTALBUKU</a>
+</footer>
+</div>
+</body>
+</html>
